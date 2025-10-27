585418beb5d78a89208e71f2c3956b48bba877b7
eadka <aditi.koppikar@gmail.com>
2025-10-27 12:27:15 +0000

    05_module homework

---
+++ 05-deployment/05_homework/marketing.py
@@ -0,0 +1,19 @@
+import requests
+
+url = 'http://localhost:9696/predict'
+
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+
+response = requests.post(url, json=client)
+convert = response.json()
+
+print("Response: ", convert)
+
+if convert['convert']>= 0.5:
+    print('Send email with promo')
+else:
+    print('dont do anything')
--- 05-deployment/05_homework/predict_pipeline.py
+++ 05-deployment/05_homework/predict_pipeline.py
@@ -1,9 +1,45 @@
 import pickle
 
+from typing import Dict, Any
 
-#open the pipeline file
+from fastapi import FastAPI
+from typing import Literal
+from pydantic import BaseModel, Field
+import uvicorn
+
+
+# --- Define input schema ---
+class LeadRecord(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+
+
+# --- Initialize app ---
+app = FastAPI(title="leadscoring-prediction")
+
+
+# --- Load pipeline ---
 with open('pipeline_v1.bin','rb') as f_in:
     pipeline = pickle.load(f_in)
+
+
+# --- Prediction function ---
+def predict_single(record: dict) -> float:
+    return float(pipeline.predict_proba([record])[0, 1])
+
+# --- API endpoint ---
+@app.post("/predict")
+def predict(record: LeadRecord):
+    prob = predict_single(record.model_dump())
+    return {
+        "leadscore_prob": prob,
+        "convert": prob >= 0.5
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=9696)
+
 
 
 record = {
@@ -11,7 +47,3 @@ record = {
     "number_of_courses_viewed": 2,
     "annual_income": 79276.0
 }
-
-pred = pipeline.predict_proba([record])
-
-print("The probablity: ", pred[0][1])
